bca01d3ced40d1bd0db2ae60f26c4c5c31b9f126
Jon Lafuente <jonlafuente99@gmail.com>
2021-11-15 19:49:27 -0500

    Added improved parallax screen in platformer world(Please see'BG' object

--- Assets/Scripts/scrollingBg.cs
+++ Assets/Scripts/scrollingBg.cs
@@ -13,48 +13,34 @@ public class scrollingBg : MonoBehaviour
     private List<GameObject> bgObjs;
 
     [Header("Variables")]
-    public bool isStaticObject;
-    public bool LoopingX;
-    [Range(0, 1)] public float xWeight;
-    [Range(0, 1)] public float yWeight;
-    
+    public GameObject cam;
+    float startX, startY, bgLen;
+    [Range(0, 1)] public float parallaxX;
+    [Range(0, 1)] public float parallaxY;
 
     private void Start()
     {
-        inputMan = GameObject.Find("[MANAGER]").GetComponent<InputManager>();
+        /*inputMan = GameObject.Find("[MANAGER]").GetComponent<InputManager>();
         playerCont = GameObject.Find("PlayerPrefab").GetComponent<PlayerController>();
         bgObjs = new List<GameObject>();
         CloneBG(new Vector3(21.25f, 0, 0));
-        CloneBG(new Vector3(-21.25f, 0, 0));
+        CloneBG(new Vector3(-21.25f, 0, 0));*/
+        startX = transform.position.x;
+        startY = transform.position.y;
+        bgLen = transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().bounds.size.x;
+        Debug.Log(transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().bounds.size);
     }
 
     private void FixedUpdate()
     {
-        //transform.position = new Vector3(playerCont.gameObject.transform.position.x, playerCont.gameObject.transform.position.y, transform.position.z);
-        MoveBGGroup();
-    }
+        float xLoop = cam.transform.position.x * (1-parallaxX);
 
-    private void MoveBGGroup()
-    {
-        float velX = -playerCont.playerRigB.velocity.x * xWeight;
-        float velY = -playerCont.playerRigB.velocity.y * yWeight;
-        foreach (GameObject g in bgObjs)
-        {
-            g.GetComponent<Rigidbody>().velocity = new Vector3(velX, velY, 0);
-            /*if (!g.GetComponent<MeshRenderer>().isVisible)
-            {
-                Debug.Log(g.name + " NOT VISIBLE");
-                //Destroy(g);
-            }*/
-        }
-    }
+        float xDist = cam.transform.position.x * parallaxX;
+        float yDist = cam.transform.position.y * parallaxY;
 
-    private void CloneBG(Vector3 startPos = default(Vector3))
-    {
-        GameObject newBG = Instantiate(bgPrefab, transform);
-        newBG.transform.localPosition = startPos;
-        newBG.GetComponent<MeshRenderer>().material = bgMat;
-        bgObjs.Add(newBG);
-        Debug.Log(newBG.GetComponent<MeshRenderer>().bounds);
+        transform.position = new Vector3(startX + xDist, startY + yDist, transform.position.z);
+
+        if (xLoop > startX + bgLen) { startX += bgLen; }
+        else if (xLoop < startX - bgLen) { startX -= bgLen; }
     }
 }
